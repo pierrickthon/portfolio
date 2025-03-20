608d6c04cf47f94ddd415ebe3d57bde22dafbf12
Yann Pierrick <154607342+pierrickthon@users.noreply.github.com>
2025-03-20 19:52:34 +0000

    Petit changement commit

--- portfolio/CvYannThon/astro.config.mjs
+++ portfolio/CvYannThon/astro.config.mjs
@@ -3,5 +3,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://pierrickthon.github.io',
+  base: 'CvYannThon',
+  trailingSlash: 'always',
   integrations: [tailwind()],
 });
